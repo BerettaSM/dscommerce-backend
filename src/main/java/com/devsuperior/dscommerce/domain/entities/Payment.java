67dae977fa23b333587cdcbff259aa7b2933a4ad
Ramon Saviato <ramonsaviato@hotmail.com>
2025-06-02 16:58:35 -0300

    add Payment entity

--- src/main/java/com/devsuperior/dscommerce/domain/entities/Payment.java
+++ src/main/java/com/devsuperior/dscommerce/domain/entities/Payment.java
@@ -2,16 +2,12 @@ package com.devsuperior.dscommerce.domain.entities;
 
 import java.time.Instant;
 
-import com.devsuperior.dscommerce.domain.enums.OrderStatus;
-
-import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.ManyToOne;
+import jakarta.persistence.MapsId;
 import jakarta.persistence.OneToOne;
 import jakarta.persistence.Table;
 import lombok.AccessLevel;
@@ -25,24 +21,19 @@ import lombok.NoArgsConstructor;
 @Entity
 @EqualsAndHashCode(onlyExplicitlyIncluded = true)
 @NoArgsConstructor(access = AccessLevel.PRIVATE, force = true)
-@Table(name = "TBL_ORDER")
-public class Order {
+@Table(name = "TBL_PAYMENT")
+public class Payment {
     
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @EqualsAndHashCode.Include
     private Long id;
-
-    // So it gets stored as UTC time zone
+    
     @Column(columnDefinition = "TIMESTAMP WITHOUT TIME ZONE")
     private Instant moment;
-    private OrderStatus status;
-
-    @ManyToOne
-    @JoinColumn(name = "client_id")
-    private User client;
-
-    @OneToOne(mappedBy = "order", cascade = CascadeType.ALL)
-    private Payment payment;
+    
+    @MapsId
+    @OneToOne
+    @EqualsAndHashCode.Include
+    private Order order;
 
 }
