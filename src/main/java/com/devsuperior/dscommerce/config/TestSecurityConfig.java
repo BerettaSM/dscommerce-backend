b74ef9e54eafe6ee07e4a80ffbc9d206489a0f60
Ramon Saviato <ramonsaviato@hotmail.com>
2025-07-15 16:57:34 -0300

    add default security configuration

---
+++ src/main/java/com/devsuperior/dscommerce/config/TestSecurityConfig.java
@@ -0,0 +1,26 @@
+package com.devsuperior.dscommerce.config;
+
+import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+import org.springframework.core.Ordered;
+import org.springframework.core.annotation.Order;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@Profile(value = "test")
+public class TestSecurityConfig {
+    
+    @Bean
+    @Order(value = Ordered.HIGHEST_PRECEDENCE)
+    SecurityFilterChain testFilterChain(HttpSecurity http) throws Exception {
+        return http
+            .securityMatcher(PathRequest.toH2Console())
+            .headers(headers -> headers.frameOptions(options -> options.disable()))
+            .csrf(csrf -> csrf.disable())
+            .build();
+    }
+
+}
